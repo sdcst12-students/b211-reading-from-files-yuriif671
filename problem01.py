888cc2abddaa2433261a77bd73f378a1ea95f36a
hungrybeagle <cyang@deltalearns.ca>
2022-10-31 10:24:09 -0700

    Create problem01.py

---
+++ problem01.py
@@ -0,0 +1,16 @@
+#!python
+"""
+Create a program to store student block schedules:
+each student has:
+  student number
+  first name
+  last name
+  blocks A-D
+  
+Your program should have a menu for people to be able to 
+call up an individual record
+display records
+edit records
+save records
+write records
+"""
