091e25c77517e2bde380499c27ab42e8342075a7
Chris Yang <cyang@deltalearns.ca>
2023-02-27 11:17:57 -0800

    Create task3.py

---
+++ task3.py
@@ -0,0 +1,7 @@
+#!python
+# Sum of Values
+
+"""
+The file task03.txt contains a lot of data.  Each cluster of data is the number of points for that particular group.  Each blank line indicates a separator before the next group.
+Read the contents of task03.txt into your program and determine the points value of the cluster with largest sum.
+"""
